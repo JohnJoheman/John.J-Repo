6ff8fbcb26758a1bc7b86bfafdfe6202a13418ae
JohnJoheman <john.joheman@elev.ga.ntig.se>
2019-09-16 20:26:08 +0200

    Skriver ut svaren som givits

--- Programmering.1/src/Class1.java
+++ Programmering.1/src/Class1.java
@@ -8,12 +8,14 @@ public class Class1 {
 		
 		System.out.println("Vad heter du?");
 		String namn = scan.next();
+		namn += scan.next();
 		
 		System.out.println("Hur gammal är du?");
 		int ålder = scan.nextInt();
 		
 		System.out.println("Vad är din adress?");
 		String adress = scan.next();
+		adress += scan.next();
 	
 		System.out.println("Vad är ditt postnummer?");
 		String postnummer = scan.next();
@@ -24,7 +26,16 @@ public class Class1 {
 		System.out.println("Vad är ditt telefonnummer?");
 		int telefonnummer = scan.nextInt();
 		
-	}	
+		
+		
+		System.out.println("Information:");
+		System.out.println("Namn:" + namn);
+		System.out.println("Ålder:" + ålder);
+		System.out.println("Adress:" + adress);
+		System.out.println("Stad:" + stad);
+		System.out.println("Telefon:" + telefonnummer);
+
+	}
 	
 }
 
