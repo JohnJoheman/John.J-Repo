d8f46a85e660fc14dd219a056ad9f6d0c25835a0
JohnJoheman <john.joheman@elev.ga.ntig.se>
2019-09-16 19:26:19 +0200

    Frågar om ålder och sparar i en variabel (ålder)

--- Programmering.1/src/Class1.java
+++ Programmering.1/src/Class1.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 
 public class Class1 {
 
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Vad heter du?");
+		String namn = scan.next();
 		
 		
 	}
