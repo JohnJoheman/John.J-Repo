c9c71585f7634d5438d5bd5f146fade8a4f4c279
JohnJoheman <john.joheman@elev.ga.ntig.se>
2019-09-29 20:23:12 +0200

    Scannar vilket tal som skrivs in och sedan skriver ut talet + Abracadabra antalet gånger som skrivits in

---
+++ Hemläxor/src/V39.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class V39 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int nummer = scan.nextInt();
+		
+		for (int i = 1; i <= nummer; i++) {
+			System.out.println(i + " Abracadabra");
+		}
+		
+	}
+}
