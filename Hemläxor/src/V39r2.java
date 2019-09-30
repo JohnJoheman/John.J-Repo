e6eb7f1edc926e7d239cd40d48aa71d016883db7
JohnJoheman <john.joheman@elev.ga.ntig.se>
2019-09-30 11:08:32 +0200

    Löst vad R2 blir

---
+++ Hemläxor/src/V39r2.java
@@ -0,0 +1,18 @@
+
+	import java.util.Scanner;
+
+	public class V39r2
+	{public static void main(String[] args) {
+		
+	
+		Scanner input = new Scanner(System.in);
+		
+		int R1 = input.nextInt();
+		int S = input.nextInt();
+		int R2 = S*2 - R1;
+		System.out.println(R2);
+
+		}
+	}
+
+
