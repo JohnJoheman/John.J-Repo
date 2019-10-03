a324bf315738bb7d04751a170e2e595d440ed123
JohnJoheman <john.joheman@elev.ga.ntig.se>
2019-10-03 10:52:44 +0200

    FizzBuzz

---
+++ Hemläxor/src/V40fizzbuzz.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class V40fizzbuzz {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int x = input.nextInt();
+		int y = input.nextInt();
+		int z = input.nextInt();
+
+		for(int i = 1; i <= z; i++) {
+			
+			if(i % x == 0 && i % y == 0) {
+				System.out.println("FizzBuzz");
+			}
+			
+			else if(i % x == 0) {
+				System.out.println("Fizz");
+			}
+				
+			else if(i % y == 0) {
+				System.out.println("Buzz");
+			}
+			
+			else if(i % x != 0 && i % y != 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
