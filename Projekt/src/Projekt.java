d596addb772d1224c753742282ee2077ca80789f
JohnJoheman <john.joheman@elev.ga.ntig.se>
2019-11-25 10:26:01 +0100

    Gjort några ändringar i ordningen, oklart hur många metoder.
    Gjort några extra metoder men ändringarna är mestadels ordningen jag lagt dem i.  Jag har råkat hoppa över vissa uppgfiter och liknande.

--- Projekt/src/Projekt.java
+++ Projekt/src/Projekt.java
@@ -20,8 +20,15 @@ public class Projekt {
 		System.out.println(pressureUnderWater(10));
 		System.out.println(kineticEnergy(2,2));
 		System.out.println(potentialEnergy(2,5));
+		System.out.println((fallSpeed(2.5)));
 		System.out.println(delta(1,10));
 		System.out.println(delta(5,1));
+		System.out.println(volumeToMass(FluidTable.WATER, 1));
+		System.out.println(volumeToMass(GasTable.AIR,1));
+		System.out.println(volumeToMass(SolidTable.IRON,1));
+		System.out.println(svtVelocity(10,5));
+		System.out.println(svtDistance(10,5));
+		
 	}
 
 
@@ -68,20 +75,47 @@ public class Projekt {
 	}
 	
 	
-	
-	/**
-	 * uppgift 7...
-	 */
-	
-	
-	
+	public static double fallSpeed(double height) {
+		double fallSpeed = Math.sqrt ((2*height) / g_swe) * g_swe;
+		System.out.println("The speed of an object from a certain height");
+		return fallSpeed;
+	}
+	 
 	public static double delta(double first, double last) {
 		double delta = last - first;
 		System.out.println("The difference between first and last number are:");
 		return delta;
 	}
 	
+	public static double volumeToMass(FluidTable fluid, double volume) {
+		double volumeToMass = fluid.density * volume;
+		System.out.println("Converts a certain volume of a liquid to a mass:");
+		return volumeToMass;
+	}
 	
+	public static double volumeToMass(GasTable gas, double volume) {
+		double volumeToMass = gas.density * volume;
+		System.out.println("Converts a certain volume of gas to mass:");
+		return volumeToMass;
+	}
+	
+	public static double volumeToMass(SolidTable solid, double volume) {
+		double volumeToMass = solid.density * volume;
+		System.out.println("Converts a certain volume of materia to mass:");
+		return volumeToMass;
+	}
+	
+	public static double svtVelocity(double distance, double time) {
+		double svtVelocity = distance / time;
+		System.out.println("The average speed:");
+		return svtVelocity;
+	}
+	
+	public static double svtDistance(double velocity, double time) {
+		double svtDistance = velocity * time;
+		System.out.println("The time");
+		return svtDistance;
+	}
 	
 	
 	
