187d6193d56ad7580905aa877b76f46347501cb9
JohnJoheman <john.joheman@elev.ga.ntig.se>
2020-03-23 17:50:12 +0100

    kod för spelet, utefter vad spelaren skriver in i konsollen
    nu har jag skrivit klart koden för spelet innan man valt svårighetsgrad. Lagt till så att beroende på vad spelaren väljer att skriva in så kommer (framtida) koden för svårighetsgraden att köras

--- Projekt/src/GuessTheNumberGame.java
+++ Projekt/src/GuessTheNumberGame.java
@@ -14,21 +14,82 @@ public class GuessTheNumberGame {
 	public static int correctNumber;
 	public static int guessedNumber;
 	public static boolean run = true;
+	public static String chooseDifficulty;
+	public static String playAgain;
 	
 
-	
+
 	public static void main(String[] args) {
 		
+		launchGuessTheNumberGame();
+	}
+	
+	public static void launchGuessTheNumberGame() {
+		
+	
+	
+	while (run) {
+		System.out.println("Hello and welcome to GuessTheNumberGame");
+		System.out.println();
+		System.out.println("You're going to guess a number thats randomly generated between a certain interval");
+		System.out.println("Easy = number between 1-10, with a total of 5 guesses");
+		System.out.println("Medium = number between 1-100, with a total of 10 guessses");
+		System.out.println("Hard = number between 1-1000, with a total of 10 guesses");
+			
+		System.out.println("Please choose a difficulty by typing it down below");
+		System.out.println("If you don't want to play, just type " + "Exit" + "down below");
+		
+		switch (chooseDifficulty) {
+		
+		case "Exit":
+			System.out.println("See you Next time!");
+			break;
+			
+		case "Easy":
+			easyDifficulty();
+			
+			break;
+			
+		case "Medium": 
+			mediumDifficulty();
+			
+			break;
+			
+		case "Hard":
+			hardDifficulty();
+		}
+		
+		
+		
+		System.out.println("Do you want to play again?");
+		System.out.println("Yes / No");
+		
+		switch (playAgain) {
+		case "Yes":
+			break;
+				
+		case "No":
+			 run = false;
+			 System.out.println("Hope to see you soon, haev a great time!");
+			
+		}
+		}
 		
 	}
-		
+	
+	
+	
+	
+	
+	
+	
+	
 		public static void theAlgorithmForGuessTheNumber()	{
 			
 			System.out.println("Let's see if you can guess the number");
 			
-		correctNumber = (int) (Math.random() * ((maxNumber-minNumber) + 1) + minNumber);
-
-		
+	
+		for(int i = 1; i <= guessesLeft; i++) {
 		if(guessedNumber == correctNumber) {
 			System.out.println("Nice Job! You've successfully guessed the right number!");
 			System.out.println();
